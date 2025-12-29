86666b32b00b6e1de8e6fa1791d509fd5b54ad84
inwoohub <dlsdndls@naver.com>
2025-12-29 20:17:15 +0900

    first commit

---
+++ src/main/java/com/hwangso/mhbackend/MhbackendApplication.java
@@ -0,0 +1,13 @@
+package com.hwangso.mhbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hwangso/mhbackend/MhbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwangso.mhbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
